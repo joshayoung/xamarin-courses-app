588830aac4a832431393783e545ddfddae4bb090
Josh Young <j22cal@gmail.com>
2021-06-21 12:20:23 -0500

    Add property change tests for viewmodel

--- CoursesApp/CoursesApp.ViewModels/CourseCollectionViewModel.cs
+++ CoursesApp/CoursesApp.ViewModels/CourseCollectionViewModel.cs
@@ -5,8 +5,10 @@ namespace CoursesApp.ViewModels
 {
     public class CourseCollectionViewModel
     {
+        public List<CourseViewModel> AllCourses => new List<CourseViewModel>();
         public CourseCollectionViewModel(List<Course> courses)
         {
+            courses.ForEach(course => AllCourses.Add(new CourseViewModel(course)));
         }
     }
 }
