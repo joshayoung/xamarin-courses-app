97fc01c98deead975ac005771941db66d0fb7231
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-06-30 15:12:33 -0500

    Add better view model collection tests

--- CoursesApp/CoursesApp.ViewModels/CourseCollectionViewModel.cs
+++ CoursesApp/CoursesApp.ViewModels/CourseCollectionViewModel.cs
@@ -1,17 +1,56 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 using CoursesApp.Models;
 
 namespace CoursesApp.ViewModels
 {
-    public class CourseCollectionViewModel
+    public class CourseCollectionViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         private readonly CourseCollection coursesCollection;
-        //public List<CourseViewModel> Courses { get; } = new List<CourseViewModel>();
-        
+
+        public CourseViewModel LastCourse => FindLastCourse();
+
+        public ObservableCollection<CourseViewModel> CoursesWithMultipleStudents { get; } =
+            new ObservableCollection<CourseViewModel>();
+
         public CourseCollectionViewModel(CourseCollection coursesCollection)
         {
             this.coursesCollection = coursesCollection;
-            //courses.ForEach(course => Courses.Add(new CourseViewModel(course)));
+            
+            coursesCollection.Courses.CollectionChanged += CoursesOnCollectionChanged;
+        }
+        
+        public void ReloadTheClasses() => coursesCollection.RepopulateCourseList();
+
+        private void CoursesOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastCourse)));
+
+                RefreshMultiStudentCourses();
+            }
+        }
+
+        private void RefreshMultiStudentCourses()
+        {
+            CoursesWithMultipleStudents.Clear();
+
+            coursesCollection.Courses.ToList().FindAll(course => course.Students.Count > 1).ToList()
+                .ForEach(rec => CoursesWithMultipleStudents.Add(new CourseViewModel(rec)));
+        }
+
+        private CourseViewModel FindLastCourse()
+        {
+            if (coursesCollection.Courses.Count > 0)
+            {
+                return new CourseViewModel(coursesCollection.Courses.Last());
+            }
+
+            return null;
         }
     }
 }
