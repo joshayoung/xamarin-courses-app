484dcb3338d78c13ee09b82ce7a8299ae316cd41
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-07-15 18:58:32 -0500

    Add formatted 'Spans' via code-behind

--- CoursesApp/CoursesApp/CoursesApp/MainPage.xaml.cs
+++ CoursesApp/CoursesApp/CoursesApp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CoursesApp.ViewModels;
 using Xamarin.Forms;
 
@@ -7,25 +9,48 @@ namespace CoursesApp
     public partial class MainPage : ContentPage
     {
         private readonly CourseCollectionViewModel courseCollectionViewModel;
+
         public MainPage(CourseCollectionViewModel courseCollectionViewModel)
         {
             InitializeComponent();
             BindingContext = this.courseCollectionViewModel = courseCollectionViewModel;
+
+            AddWelcomeText();
+        }
+
+        private void AddWelcomeText()
+        {
+            var label = new Label {FontSize = 24};
+            var formattedString = new FormattedString();
+            var spanInitial = new Span {Text = "Welcome to the"};
+            var spanPenultimate = new Span
+            {
+                Text = " Courses",
+                TextColor = Color.Teal,
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 24
+            };
+            var spanFinal = new Span {Text = " App. Select a course below to see the details for the class."};
+            formattedString.Spans.Add(spanInitial);
+            formattedString.Spans.Add(spanPenultimate);
+            formattedString.Spans.Add(spanFinal);
+            label.FormattedText = formattedString;
+            Welcome.Children.Add(label);
         }
 
         private void NavigateToDetails(object sender, EventArgs e)
         {
-            var frame = (Frame)sender;
+            var frame = (Frame) sender;
             frame.BackgroundColor = Color.Coral;
-            
-            if (!(((VisualElement)sender).BindingContext is CourseViewModel courseViewModel)) return;
+
+            if (!(((VisualElement) sender).BindingContext is CourseViewModel courseViewModel)) return;
             Navigation.PushAsync(new StatsPage(courseViewModel));
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            
+
             courseCollectionViewModel.ReloadTheClasses();
         }
     }
