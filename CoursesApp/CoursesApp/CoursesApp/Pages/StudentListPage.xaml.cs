b371ab1bf33e07f981fae79c6162575ffe1263ac
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-07-17 15:17:38 -0500

    Cleanup page content and styling

---
+++ CoursesApp/CoursesApp/CoursesApp/Pages/StudentListPage.xaml.cs
@@ -0,0 +1,23 @@
+using System;
+using CoursesApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoursesApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StudentListPage : ContentPage
+    {
+        public StudentListPage(CourseViewModel courseViewModel)
+        {
+            InitializeComponent();
+            BindingContext = courseViewModel;
+        }
+
+        private void ViewStudentDetails(object sender, EventArgs e)
+        {
+            if (!(((VisualElement)sender).BindingContext is StudentViewModel studentViewModel)) return;
+            Navigation.PushAsync(new StudentPage(studentViewModel));
+        }
+    }
+}
