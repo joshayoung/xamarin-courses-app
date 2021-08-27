0e6f4c94431c79e46c3980027e1b20a1c1b45b2f
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-08-27 12:41:36 -0500

    Add/Remove 'CourseViewModel' tests

--- CoursesApp/CoursesApp.Models.Test/CourseTest.cs
+++ CoursesApp/CoursesApp.Models.Test/CourseTest.cs
@@ -42,30 +42,6 @@ namespace CoursesApp.Models.Test
 
 
         [Fact]
-        public void UpdateAverageAge_Called_ExpectCorrectResults()
-        {
-            var courseDataService = Substitute.ForPartsOf<CourseDataService>();
-            var courseCollection = new CourseCollection(courseDataService)
-            {
-                Students = { new Student(1) },
-            };
-            var course = new Course("1", "title", 2, CourseType.Discussion, new List<int>() { 1 });
-            courseCollection.Courses.Add(course);
-            var wasAverageUpdated = false;
-            
-
-            course.PropertyChanged += (sender, args) =>
-            {
-                if (args.PropertyName == nameof(Course.AverageStudentAge)) wasAverageUpdated = true;
-            };
-            
-            course.UpdateAverageAge(courseCollection);
-
-            wasAverageUpdated.Should().BeTrue();
-        }
-
-
-        [Fact]
         public void PropertiesChange_Called_ExpectPropertyChangedEvent()
         {
             var wasTitleChanged = false;
@@ -91,6 +67,56 @@ namespace CoursesApp.Models.Test
             wasLengthChanged.Should().BeTrue();
             wasTypeChanged.Should().BeTrue();
             wasStudentsChanged.Should().BeTrue();
+        }
+
+        [Fact]
+        public void UpdateAverageAge_Called_PropertyChangeForAverageStudentAge()
+        {
+            var courseDataService = Substitute.ForPartsOf<CourseDataService>();
+            var courseCollection = new CourseCollection(courseDataService)
+            {
+                Students = { new Student(1) },
+            };
+            var course = new Course("1", "title", 2, CourseType.Discussion, new List<int>() { 1 });
+            courseCollection.Courses.Add(course);
+            var wasAverageUpdated = false;
+            course.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == nameof(Course.AverageStudentAge)) wasAverageUpdated = true;
+            };
+
+            course.UpdateAverageAge(courseCollection);
+
+            wasAverageUpdated.Should().BeTrue();
+        }
+
+        [Fact]
+        public void AverageStudentAge_Called_ExpectAverageAgeReturned()
+        {
+            var courseDataService = Substitute.ForPartsOf<CourseDataService>();
+            var courseCollection = Substitute.ForPartsOf<CourseCollection>(courseDataService);
+            courseCollection.Students.Add(new Student(1, "Joe", 20, "Math"));
+            courseCollection.Students.Add(new Student(2, "Sally", 30, "Math"));
+            var students = new List<int>() { 1, 2 };
+            var course = new Course("1", "title", 2, CourseType.Lab, students);
+
+            course.UpdateAverageAge(courseCollection);
+            var results = course.AverageStudentAge;
+
+            results.Should().Be(25);
+        }
+        
+        [Fact]
+        public void AverageStudentAge_NoStudents_ExpectZeroReturned()
+        {
+            var courseDataService = Substitute.ForPartsOf<CourseDataService>();
+            var courseCollection = Substitute.ForPartsOf<CourseCollection>(courseDataService);
+            var course = new Course("1", "title", 2, CourseType.Lab, null);
+
+            course.UpdateAverageAge(courseCollection);
+            var results = course.AverageStudentAge;
+
+            results.Should().Be(0);
         }
     }
 }
