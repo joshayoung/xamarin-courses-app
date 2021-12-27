2d26c9f55da69ba177ef3270ff1b45b48a7fe035
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-12-27 14:19:06 -0600

    Remove unneeded Student model tests

--- CoursesApp/CoursesApp.Models.Test/StudentTest.cs
+++ CoursesApp/CoursesApp.Models.Test/StudentTest.cs
@@ -6,35 +6,6 @@ namespace CoursesApp.Models.Test
     public class StudentTest
     {
         [Fact]
-        public void Constructor_DefaultParams_ExpectAssignment()
-        {
-            const int id = 1;
-        
-            var student = new Student(id);
-        
-            student.Id.Should().Be(id);
-            student.Name.Should().Be("");
-            student.Age.Should().Be(0);
-            student.Major.Should().Be("");
-        }
-        
-        [Fact]
-        public void Constructor_NonDefaultParams_ExpectAssignment()
-        {
-            const int id = 1;
-            const string name = "name";
-            const int age = 20;
-            const string major = "major";
-        
-            var student = new Student(id, name, age, major);
-        
-            student.Id.Should().Be(id);
-            student.Name.Should().Be(name);
-            student.Age.Should().Be(age);
-            student.Major.Should().Be(major);
-        }
-        
-        [Fact]
         public void Properties_Changed_ExpectPropertyChangedEvent()
         {
             var wasNameChanged = false;
